c4a6617188d76e144132433024a492001594f924
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2024-09-15 12:39:30 +0800

    feat: Starting landing page structure

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import logo from "@/assets/logo.png";
+import { Button } from "@/components/ui/button";
+
+const SocialLink = ({ href, label }: { href: string; label: string }) => {
+  return (
+    <Button asChild className="w-full h-16 rounded-[24px]">
+      <a href={href} target="_blank" rel="noreferrer">
+        {label}
+      </a>
+    </Button>
+  );
+};
+
+const App = () => {
+  return (
+    <main className="flex items-center flex-col">
+      <img src={logo} className="mx-auto w-full" alt="Charsibel logo" />
+      <div className="flex flex-col items-center justify-center gap-4 py-10 w-1/2">
+        <SocialLink href="mailto:hello@charsibel.com" label="Email" />
+        <SocialLink href="https://twitch.tv/charsibel" label="Twitch" />
+        <SocialLink
+          href="https://instagram.com/_maddieedwards"
+          label="Instagram"
+        />
+        <SocialLink
+          href="https://youtube.com/@hellocharsibel"
+          label="YouTube"
+        />
+        <SocialLink href="https://tiktok.com/@charsibel" label="TikTok" />
+        <SocialLink href="https://throne.com/charsibel" label="Throne" />
+      </div>
+    </main>
+  );
+};
+
+export default App;
