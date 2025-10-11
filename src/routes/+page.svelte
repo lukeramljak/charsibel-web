3b79b2ef1d447a5407dd6b6748137905cc272885
Luke Ramljak <lukeramljak@proton.me>
2025-10-11 10:43:10 +0800

    refactor: move main to layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,5 @@
   import SocialLinks from "$lib/components/social-links.svelte";
 </script>
 
-<main>
-  <img src="/logo.png" alt="Charsibel logo" class="logo" />
-  <SocialLinks />
-</main>
+<img src="/logo.png" alt="Charsibel logo" class="logo" />
+<SocialLinks />
