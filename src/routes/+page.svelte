6a111b93ea88638832604a6246739b8d2ca74dc4
Luke Ramljak <lukeramljak@proton.me>
2025-10-11 10:25:29 +0800

    refactor: move styles to app.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,20 +6,3 @@
   <img src="/logo.png" alt="Charsibel logo" class="logo" />
   <SocialLinks />
 </main>
-
-<style>
-  main {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 1rem;
-    gap: 2rem;
-    margin: 0 auto;
-    max-width: 500px;
-  }
-
-  .logo {
-    display: block;
-    max-width: 100%;
-  }
-</style>
